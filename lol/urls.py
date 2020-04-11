54b7f48c0f271dd610629199fd2627ccd7761a43
themaleem <zizo9585@gmail.com>
2020-04-11 01:20:10 +0100

    minor fixes

--- lol/urls.py
+++ lol/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.LandingView.as_view() , name='landing' ),
     path('notes/', include('todo.urls',namespace='notes')),
-    path('urlshortner', include('urlshortner.urls',namespace='urlshortner')),
+    path('urlshortner/', include('urlshortner.urls',namespace='urlshortner')),
     path('accounts/login/', views.login_view, name='login'),
     path('accounts/logout/', views.logout_view, name='logout'),
     path('accounts/register/', views.register_view, name='register'),
--- urlshortner/views.py
+++ urlshortner/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Link
 from django.views import View
-from .forms import CreateLinkForm
+from .forms import CreateLinkForm,
 # Create your views here.
 from django.contrib import messages
 
@@ -11,7 +11,8 @@ class CreateShortenedLink(View):
     #get link and sortened link
     form = CreateLinkForm()
     template_name = 'create_url.html'
-    context = {'form':form}
+    links=Link.objects.filter(owner=request.user)
+    context = {'form':form,'links':links}
     return render(request,template_name,context)
 
   def post(self,request):
@@ -20,13 +21,9 @@ class CreateShortenedLink(View):
     if form.is_valid():
         niickname = form.cleaned_data['niickname']
         url = form.cleaned_data['url']
-        link = Link.objects.create(niickname=niickname, url=url)
+        link = Link.objects.create(niickname=niickname, url=url,owner=request.user)
         messages.success(request, 'Your url has been shortened and can be accessed via this link  http://127.0.0.1:8000/{}'.format(link.attach))
         return redirect('urlshortner:shortner')
-
-
-
-
 class RedirectLink(View):
   def get(self,request,attach=None):
     # i should add a try nd except catchto handle errors here
