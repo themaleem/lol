2178431fa8740ac3491c1b4bf4204b3f20de293d
themaleem <zizo9585@gmail.com>
2020-04-11 01:20:10 +0100

    minor fixes

--- urlshortner/models.py
+++ urlshortner/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 import random, string
@@ -8,9 +8,10 @@ def randomword(length):
    return ''.join(random.choice(letters) for i in range(length))
 
 class Link(models.Model):
-  attach  = models.CharField(max_length=100)
+  attach  = models.CharField(max_length=100,unique=True)
   niickname = models.CharField(max_length=20)
   url= models.URLField(max_length=200)
+  owner=models.ForeignKey(to=settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='links')
 
   def save(self,*args,**kwargs):
     self.attach = randomword(10)
